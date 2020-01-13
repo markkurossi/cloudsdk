fee371290509d76d4b6203d741714750e27beed1
Markku Rossi <mtr@iki.fi>
2020-01-13 17:03:32 -0500

    Initial work on request authorization.

--- api/auth/token.go
+++ api/auth/token.go
@@ -9,9 +9,30 @@
 package auth
 
 import (
+	"fmt"
+	"net/http"
+
 	"golang.org/x/crypto/ed25519"
 )
 
 func VerifyToken(token []byte, pub ed25519.PublicKey) bool {
 	return false
+}
+
+func Authorize(w http.ResponseWriter, r *http.Request, realm string,
+	tokenVerifier func(token string) bool) bool {
+
+	auth := r.Header.Get("Authorization")
+	if len(auth) == 0 {
+		Error401(w, realm)
+		return false
+	}
+	// XXX
+
+	return true
+}
+
+func Error401(w http.ResponseWriter, realm string) {
+	w.Header().Set("WWW-Authenticate", fmt.Sprintf(`Bearer realm="%s"`, realm))
+	w.WriteHeader(http.StatusUnauthorized)
 }
