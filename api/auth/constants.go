724b34dd56c05da11e8cfae3fbcbd3e10d635335
Markku Rossi <mtr@iki.fi>
2020-01-20 13:26:26 -0500

    Token values and symtab.

--- api/auth/constants.go
+++ api/auth/constants.go
@@ -17,12 +17,46 @@ const (
 	TOKEN_SIGNATURE
 	T_TENANT_ID
 	T_CLIENT_ID
-	T_CREATED
+	T_NOT_BEFORE
+	T_NOT_AFTER
 	T_SCOPE
 )
 
 const (
 	SCOPE_ADMIN tlv.Type = iota
+)
+
+var (
+	TokenSymtab = tlv.Symtab{
+		TOKEN_VALUES: tlv.Symbol{
+			Name: "values",
+			Child: tlv.Symtab{
+				T_TENANT_ID: tlv.Symbol{
+					Name: "tenant_id",
+				},
+				T_CLIENT_ID: tlv.Symbol{
+					Name: "client_id",
+				},
+				T_NOT_BEFORE: tlv.Symbol{
+					Name: "not_before",
+				},
+				T_NOT_AFTER: tlv.Symbol{
+					Name: "not_after",
+				},
+				T_SCOPE: tlv.Symbol{
+					Name: "scope",
+					Child: tlv.Symtab{
+						SCOPE_ADMIN: tlv.Symbol{
+							Name: "admin",
+						},
+					},
+				},
+			},
+		},
+		TOKEN_SIGNATURE: tlv.Symbol{
+			Name: "signature",
+		},
+	}
 )
 
 const (
